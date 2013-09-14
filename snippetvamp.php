0cecb61cbe3146f35cc1530e1c8eb2746dfd89cb
Phyks <webmaster@phyks.me>
2013-09-14 15:02:00 +0200

    Added a javascript function to handle tabs in textarea.

--- snippetvamp.php
+++ snippetvamp.php
@@ -812,6 +812,28 @@ else{echo $contenu;}
         });
         
     });
+
+    document.querySelector("textarea").addEventListener('keydown',function(e) {
+        if(e.keyCode === 9) { // tab was pressed
+            // get caret position/selection
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
+
+            var target = e.target;
+            var value = target.value;
+
+            // set textarea value to: text before caret + tab + text after caret
+            target.value = value.substring(0, start)
+                        + "\t"
+                        + value.substring(end);
+
+            // put caret at right position again (add one for the tab)
+            this.selectionStart = this.selectionEnd = start + 1;
+
+            // prevent the focus lose
+            e.preventDefault();
+        }
+    },false);
 </script>
 </body> 
 </html>
